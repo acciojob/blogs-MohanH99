a8861f9a7423a0367263bb0e396494c01b799c30
“MohanH99” <mohanhansda2@gmail.com>
2023-02-19 00:48:31 +0530

    Soln

--- src/main/java/com/driver/controller/ImageController.java
+++ src/main/java/com/driver/controller/ImageController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.Exception;
+
 @RestController
 @RequestMapping("/images")
 public class ImageController {
